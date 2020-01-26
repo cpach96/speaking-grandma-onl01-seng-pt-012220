306b5217dc27e0e4704a994752550c73b8fa4bd8
Curtis pachtinger <cpach96@gmail.com>
2020-01-26 23:17:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,8 @@
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return ""
   end
 end
   
